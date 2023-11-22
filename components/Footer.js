3a6dc5161f87bfcbcf50c339f0ed03a3bf27bc52
Fazlul-Karim-Shahed <fazlul.karim1@northsouth.edu>
2023-11-23 01:22:59 +0600

    bug fix

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,13 @@
 import Link from "next/link";
 
 const Footer = () => {
-    return ( 
+    return (
         <div className="py-4">
             <div className="text-center small">
-                Copyright@2023. All right reserved <Link className="fst-italic" href='fazlul-karim.web.app'>Fazlul Karim</Link>
+                Copyright@2023. All right reserved <Link target="_blank" className="fst-italic" href='https://fazlul-karim.web.app/'>Fazlul Karim</Link>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Footer;
